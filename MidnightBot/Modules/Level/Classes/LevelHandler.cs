bb91a0d4ef64febd939ab6009e450a99bab8540e
Jannik Müller <jannik@andreasmueller.de>
2016-08-17 18:51:38 +0200

    Try to fix level crash bug.

--- MidnightBot/Modules/Level/Classes/LevelHandler.cs
+++ MidnightBot/Modules/Level/Classes/LevelHandler.cs
@@ -15,38 +15,50 @@ namespace MidnightBot.Modules.Level.Classes
 
         public async void levelChanged(object sender, LevelChangedEventArgs e)
         {
-            long uid = Convert.ToInt64(e.User.Id);
-
-            int level = e.NewLevel;
-
-            IEnumerable<KeyValuePair<int, string>> enumerable = MidnightBot.Config.LevelRanks.Where(i => i.Key == level);
-
-            if(enumerable != null)
+            try
             {
-                if(enumerable.Count() > 0)
+                long uid = Convert.ToInt64(e.User.Id);
+
+                int level = e.NewLevel;
+
+                IEnumerable<KeyValuePair<int, string>> enumerable = MidnightBot.Config.LevelRanks.Where(i => i.Key == level);
+
+                if (enumerable != null)
                 {
-                    KeyValuePair<int, string> keyValue = enumerable.First();
-
-                    string rankId = keyValue.Value;
-
-                    Role role = e.Channel.Server.FindRoles(rankId).First();
-
-                    if(role != null)
+                    if (enumerable.Count() > 0)
                     {
-                        try
-                        {
-                            await e.User.AddRoles(e.Channel.Server.FindRoles(rankId).FirstOrDefault());
+                        KeyValuePair<int, string> keyValue = enumerable.First();
 
-                            await e.Channel.SendMessage($"Herzlichen Glückwunsch { e.User.Mention }, du bist nun { e.Channel.Server.FindRoles(rankId).FirstOrDefault().Mention }");
-                        } catch
+                        string rankId = keyValue.Value;
+
+                        Role role = e.Channel.Server.FindRoles(rankId).First();
+
+                        if (role != null)
                         {
-                            Console.WriteLine("Bot has insufficient rights.");
+                            if (e.User.HasRole(role))
+                                return;
+
+                            try
+                            {
+                                await e.User.AddRoles(e.Channel.Server.FindRoles(rankId).FirstOrDefault()).ConfigureAwait(false);
+
+                                await e.Channel.SendMessage($"Herzlichen Glückwunsch { e.User.Mention }, du bist nun { e.Channel.Server.FindRoles(rankId).FirstOrDefault().Mention }").ConfigureAwait(false);
+                            }
+                            catch (Exception ex)
+                            {
+                                Console.WriteLine($"Bot has insufficient rights. { ex.Message }");
+                            }
                         }
-                    } else
-                    {
-                        Console.WriteLine($"Role { rankId } does not exist.");
+                        else
+                        {
+                            Console.WriteLine($"Role { rankId } does not exist.");
+                        }
                     }
                 }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
             }
         }
     }
